2a30ffe8d99a92f4c75b4f2b470e41e3037fe3c2
Ruben Requena <rubnrequena@gmail.com>
2020-09-23 09:43:32 +0200

    mes y anio en serial

--- utils/ticket-util.js
+++ utils/ticket-util.js
@@ -1,5 +1,5 @@
 const redisRepo = require("../repositorio/redis-repo");
-const { diaDelAnio } = require("./date-util");
+const { trailZero } = require("./number-util");
 
 const anioInicio = 2020;
 const letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
@@ -10,8 +10,10 @@ module.exports = {
   async ticketSerial() {
     let now = new Date(); //FIXME evitar tener que consultar la fecha cada momento
     let anio = letras[now.getFullYear() - anioInicio];
-    let hora = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
-    const serial = `${anio}${diaDelAnio()}${hora}`;
+    //let hora = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
+    let mes = trailZero(now.getMonth() + 1);
+    let dia = trailZero(now.getDate());
+    const serial = `${anio}${mes}${dia}`;
     numero = await redisRepo.incrementar(serial);
     return `${serial}${numero}`;
   },
