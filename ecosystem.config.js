aa176a3bbd74deb0c98d4ef60b258f83eb2adc30
Ruben Requena <rubnrequena@gmail.com>
2020-11-14 17:39:41 +0100

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,14 @@ module.exports = {
       ignore_watch: ["node_modules", "tests"],
       instances: 4,
       exec_mode: "cluster",
+      env: {
+        PORT: 3000,
+        NODE_ENV: "development",
+      },
+      env_production: {
+        PORT: 443,
+        NODE_ENV: "production",
+      },
     },
   ],
 };
