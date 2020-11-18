04c1eb68e767bd1e28f5af82a2af7b633a137a40
Ruben Requena <rubnrequena@gmail.com>
2020-11-18 19:46:16 +0100

    test

--- tests/taquilla.js
+++ tests/taquilla.js
@@ -120,13 +120,13 @@ describe("premiar", () => {
   it("reiniciar sorteo", async function () {
     const operadora = operadoras[0];
     const sorteo = operadora.sorteos[operadora.sorteos.length - 1];
-    return sorteoService.reiniciar(sorteo._id);
+    return await sorteoService.reiniciar(sorteo._id);
   });
   it("premiar sorteo", async function () {
     this.timeout(0);
     const operadora = operadoras[0];
     const sorteo = operadora.sorteos[operadora.sorteos.length - 1];
-    return sorteoService.premiar(sorteo._id, getRandomInt(0, 36));
+    return await sorteoService.premiar(sorteo._id, getRandomInt(0, 36));
   });
 });
 
