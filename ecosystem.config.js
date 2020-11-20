d7b766f14c8ad214f43111d8762f12f1da240736
Ruben Requena <rubnrequena@gmail.com>
2020-11-20 01:45:00 +0100

    variames de entorno en pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,29 @@
 module.exports = {
-  apps: [
+  apps = [
     {
       name: "caribe",
       script: "bin/www",
       watch: false,
       ignore_watch: ["node_modules", "tests"],
-      instances: 4,
+      instances: 2,
       exec_mode: "cluster",
       env: {
-        PORT: 3000,
-        NODE_ENV: "development",
+        PORT: 4000,
+        MONGODB_URI: "mongodb://localhost/fullapuesta",
+        MONGODB_URI_DEV: "mongodb://localhost/fullapuesta",
+        SMTP_USUARIO:"no-responder@sistemasrq.com",
+        SMTP_CLAVE:"srq@s0p0rt3",
+        SMTP_HOST:"mail.sistemasrq.com",
+        SMTP_PORT:465,
+        REDIS_PORT:0,
+        NODE_ENV: "development"
       },
       env_production: {
         PORT: 443,
+        MONGODB_URI: "mongodb://localhost/fullapuesta",
+        MONGODB_URI_DEV: "mongodb://localhost/fullapuesta",
         NODE_ENV: "production",
-      },
+      }
     },
   ],
 };
